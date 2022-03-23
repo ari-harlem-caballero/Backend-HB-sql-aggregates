481b9693a28dae166ea2a888e186148ce80175c2
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-23 12:56:45 -0700

    finished exercise 4

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,19 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT
+  category.name,
+  AVG(length) AS avg
+FROM
+  film_category
+INNER JOIN
+  film
+ON
+  film.film_id = film_category.film_id
+INNER JOIN
+  category
+ON
+  category.category_id = film_category.category_id
+GROUP BY
+  category.name
+-- ORDER BY
+--   avg DESC;
